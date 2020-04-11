aac711a01f4d583525841dfbb995b0ec0c3eb0bc
Nícolas Yamakoshi <nicolas@jungledevs.com>
2020-04-11 01:55:49 -0300

    refactor: Uncomment whitenoise settings following heroku's guidelines.

--- settings/settings.py
+++ settings/settings.py
@@ -158,8 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ###
 # Storage
