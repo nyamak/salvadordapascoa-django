cf78ec2106016fd991038d5e9c7c36a5d9b8eca2
Nícolas Yamakoshi <nicolas@jungledevs.com>
2020-04-15 23:14:58 -0300

    feat: Add helper script.

--- helpers/from_csv_to_db.py
+++ helpers/from_csv_to_db.py
@@ -1,6 +1,7 @@
 """
 From CSV to DB migration script
 """
+from collections import defaultdict
 
 import pandas as pd
 
@@ -8,15 +9,22 @@ from seller.constants import FRIENDS, INSTAGRAM, OTHERS
 from seller.models import Seller, DeliveryMean, OrderMean, ProductImage
 
 
+def is_nan(val):
+    if str(val) == 'nan':
+        return None
+    else:
+        return val
+
+
 print('Opening CSV...')
-df = pd.read_csv('~/sellers.csv')
+df = pd.read_csv('sellers.csv')
 print('Successfully opened CSV...')
 
 
 print('Creating auxiliary variables...')
 # Lists to store objs to create
 sellers_to_create = []
-images_to_create = []
+images_to_create = defaultdict(list)
 
 # DeliveryMeans data structs
 delivery_table = {
@@ -44,7 +52,7 @@ referrals_table = {
     'Outras formas': OTHERS,
 }
 
-existing_sellers_profiles = Seller.objects.all().values_list('instagram_profile', flat=True)
+existing_sellers_profiles = list(Seller.objects.all().values_list('instagram_profile', flat=True))
 
 print('Starting loop...')
 for index, row in df.iterrows():
@@ -53,49 +61,65 @@ for index, row in df.iterrows():
     if row['instagram'] in existing_sellers_profiles:
         print(f'Seller {row["instagram"]} already in DB, continuing...')
         continue
+    else:
+        existing_sellers_profiles.append(row['instagram'])
 
+    print(row['accepted'])
     # Seller declaration
     seller = Seller(
         name=row['name'],
         description=row['obs'],
         neighborhood=row['neighborhood'],
-        city=row['Cidade (se aceito)'],
-        state=row['state'],
+        city=is_nan(row['city']),
+        state=is_nan(row['state']),
         telephone_number=row['phoneNumber'],
-        whatsapp=row['whatsapp'],
-        site_url=row['site'],
+        whatsapp_number=is_nan(row['whatsapp']),
+        site_url=is_nan(row['site']),
         instagram_profile=row['instagram'],
-        ifood_url=row['Ifood'],
-        uber_eats_url=row['UberEats'],
-        rappi_url=row['Rappi'],
+        ifood_url=is_nan(row['Ifood']),
+        uber_eats_url=is_nan(row['UberEats']),
+        rappi_url=is_nan(row['Rappi']),
         referrals=referrals_table[row['Como você nos achou?']] if row['Como você nos achou?'] in referrals_table.keys() else None,
-        cover_image=row['photo'],
-        is_approved=True if row['accepted'] == 'TRUE' else False
+        cover_image=is_nan(row['photo']),
+        is_approved=True if str(row['accepted']) == 'True' else False
     )
 
-    row_delivery_means = row['howToReceive'].split(', ')
+    row_delivery_means = row['howToReceive'].split(', ') if str(row['howToReceive']) != 'nan' else []
     for mean in row_delivery_means:
-        delivery_dict[mean].append(seller)
+        try:
+            delivery_dict[mean].append(seller)
+        except KeyError:
+            continue
 
-    row_order_means = row['delivery'].split(', ')
+    row_order_means = row['delivery'].split(', ') if str(row['delivery']) != 'nan' else []
     for mean in row_order_means:
-        order_dict[mean].append(seller)
+        try:
+            order_dict[mean].append(seller)
+        except KeyError:
+            continue
 
-    row_images = row['allPhotos'].split(', ')
+    row_images = row['allPhotos'].split(', ') if str(row['allPhotos']) != 'nan' else []
     for idx, image_url in enumerate(row_images):
         image = ProductImage(
-            seller=seller,
             order=idx,
             image=image_url
         )
-        images_to_create.append(image)
+        images_to_create[seller.instagram_profile].append(image)
 
     sellers_to_create.append(seller)
 
 print('Finished processing Sellers.\nBulk creating Sellers...')
 Seller.objects.bulk_create(sellers_to_create)
-print('Finished bulk creating Sellers.\nBulk creating ProductImages...')
-ProductImage.objects.bulk_create(images_to_create)
+print('Finished bulk creating Sellers.\n Creating ProductImages...')
+final_images_to_create = []
+sellers = list(Seller.objects.all())
+
+for seller in sellers:
+    if seller.instagram_profile in images_to_create:
+        for image in images_to_create[seller.instagram_profile]:
+            image.seller = seller
+            final_images_to_create.append(image)
+ProductImage.objects.bulk_create(final_images_to_create)
 print('Finished bulk creating ProductImages.')
 
 
@@ -103,7 +127,7 @@ print('Attaching Sellers to OrderMeans...')
 order_means = OrderMean.objects.all()
 for mean in order_means:
     try:
-        mean.sellers.add(order_dict[order_table[mean.slug]])
+        mean.sellers.add(*order_dict[order_table[mean.slug]])
     except KeyError:
         continue
 
@@ -111,7 +135,7 @@ print('Finished attaching Sellers to OrderMeans.\nAttaching Sellers to DeliveryM
 delivery_means = DeliveryMean.objects.all()
 for mean in delivery_means:
     try:
-        mean.sellers.add(delivery_dict[delivery_table[mean.slug]])
+        mean.sellers.add(*delivery_dict[delivery_table[mean.slug]])
     except KeyError:
         continue
 print('Finished attaching Sellers to DeliveryMeans.')
